385d35033b2b0fbd6c92d586fadc030a504c32a5
Nimra Khan <72073029+nimmiee@users.noreply.github.com>
2022-02-03 18:32:48 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\alphatron-9bda-Web-Deploy.pubxml -p:Password=9qbAdzhA3qNSxT13WrS8TYgCAJ4N0kKq1EKvuN7f4lg0Fl1cnPNoehtNFlrw
+
